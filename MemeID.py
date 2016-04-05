ae3a47a5a7c7d0fcefd5b8881f682ea7aa270c91
Lucas Beltrame <lucas.beltrame@he-arc.ch>
2016-04-05 17:38:47 +0200

    Passage à Python 3.5 (plus simple pour installer par la suite)
    Codage de l'exemple de la doc

--- MemeID.py
+++ MemeID.py
@@ -1,90 +1,30 @@
 import cv2
 import numpy as np
-import itertools
-import sys
+from matplotlib import pyplot as plt
 
-#Source du code : http://www.jayrambhia.com/blog/sift-keypoint-matching-using-python-opencv/
+img = cv2.imread('img/memes/a8b.jpg',0)
+imgQuery = cv2.imread('img/templates/doge.jpg',0)
 
-def findKeyPoints(img, template, distance=200):
-    detector = cv2.FeatureDetector_create("SIFT")
-    descriptor = cv2.DescriptorExtractor_create("SIFT")
+# Initiate SIFT detector
+sift = cv2.xfeatures2d.SIFT_create()
 
-    skp = detector.detect(img)
-    skp, sd = descriptor.compute(img, skp)
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img,None)
+kp2, des2 = sift.detectAndCompute(imgQuery,None)
 
-    tkp = detector.detect(template)
-    tkp, td = descriptor.compute(template, tkp)
+# BFMatcher with default params
+bf = cv2.BFMatcher()
+matches = bf.knnMatch(des1,des2, k=2)
 
-    flann_params = dict(algorithm=1, trees=4)
-    flann = cv2.flann_Index(sd, flann_params)
-    idx, dist = flann.knnSearch(td, 1, params={})
-    del flann
+# Apply ratio test
+good = []
+for m,n in matches:
+    if m.distance < 0.75*n.distance:
+        good.append([m])
+# cv2.drawMatchesKnn expects list of lists as matches.
+img3 = cv2.drawMatchesKnn(img,kp1,imgQuery,kp2,good,None,flags=2)
+print(good.__len__())
+print(matches.__len__())
 
-    dist = dist[:,0]/2500.0
-    dist = dist.reshape(-1,).tolist()
-    idx = idx.reshape(-1).tolist()
-    indices = range(len(dist))
-    indices.sort(key=lambda i: dist[i])
-    dist = [dist[i] for i in indices]
-    idx = [idx[i] for i in indices]
-    skp_final = []
-    for i, dis in itertools.izip(idx, dist):
-        if dis < distance:
-            skp_final.append(skp[i])
-
-    flann = cv2.flann_Index(td, flann_params)
-    idx, dist = flann.knnSearch(sd, 1, params={})
-    del flann
-
-    dist = dist[:,0]/2500.0
-    dist = dist.reshape(-1,).tolist()
-    idx = idx.reshape(-1).tolist()
-    indices = range(len(dist))
-    indices.sort(key=lambda i: dist[i])
-    dist = [dist[i] for i in indices]
-    idx = [idx[i] for i in indices]
-    tkp_final = []
-    for i, dis in itertools.izip(idx, dist):
-        if dis < distance:
-            tkp_final.append(tkp[i])
-
-    return skp_final, tkp_final
-
-def drawKeyPoints(img, template, skp, tkp, num=-1):
-    h1, w1 = img.shape[:2]
-    h2, w2 = template.shape[:2]
-    nWidth = w1+w2
-    nHeight = max(h1, h2)
-    hdif = (h1-h2)/2
-    newimg = np.zeros((nHeight, nWidth, 3), np.uint8)
-    newimg[hdif:hdif+h2, :w2] = template
-    newimg[:h1, w2:w1+w2] = img
-
-    maxlen = min(len(skp), len(tkp))
-    if num < 0 or num > maxlen:
-        num = maxlen
-    for i in range(num):
-        pt_a = (int(tkp[i].pt[0]), int(tkp[i].pt[1]+hdif))
-        pt_b = (int(skp[i].pt[0]+w2), int(skp[i].pt[1]))
-        cv2.line(newimg, pt_a, pt_b, (255, 0, 0))
-    return newimg
-
-
-def match():
-    img = cv2.imread('img/315.jpg')
-    temp = cv2.imread('img/doge.jpg')
-    try:
-    	dist = 50
-    except IndexError:
-    	dist = 200
-    try:
-    	num = -1
-    except IndexError:
-    	num = -1
-    skp, tkp = findKeyPoints(img, temp, dist)
-    newimg = drawKeyPoints(img, temp, skp, tkp, num)
-    cv2.imshow("image", newimg)
-    cv2.waitKey(0)
-
-
-match()
+plt.imshow(img3)
+plt.show()
