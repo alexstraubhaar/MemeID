a6c9befe4d45051d1666a2c636994812df81ca93
Alex <alexandre.straubhaar@he-arc.ch>
2016-04-26 16:51:46 +0200

    Tout dans une jolie classe <3

--- MemeID.py
+++ MemeID.py
@@ -1,100 +1,105 @@
 import cv2
-import tkinter as tk
-from tkinter.filedialog import *
 import numpy as np
-from matplotlib import pyplot as plt
 import glob, os
+import webbrowser
+import tkinter as tk
 
+from matplotlib import pyplot as plt
+from tkinter import PhotoImage
+from tkinter.filedialog import *
 from PIL import Image
 from PIL import ImageTk as tki
-from tkinter import PhotoImage
 
-def traitement(img, imgQuery):
-    # Initiate SIFT detector
-    sift = cv2.xfeatures2d.SIFT_create()
+class MainApp:
 
-    # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img,None)
-    kp2, des2 = sift.detectAndCompute(imgQuery,None)
+    def __init__(self, root):
+        #Barre de menu
+        self.menubar = Menu(root)
+        self.menu1 = Menu(self.menubar, tearoff=0)
+        self.menu1.add_command(label="Ouvrir...", command=self.work)
+        self.menu1.add_separator()
+        self.menu1.add_command(label="Quitter", command=root.quit)
+        self.menubar.add_cascade(label="Fichier", menu=self.menu1)
+        # un vrai bouton ça serait bien
+        self.menu1.add_command(label="So dank", command=self.knowYourDank)
+        self.labelImage = Label(root)
+        self.labelImage.pack(fill=BOTH)
 
-    # BFMatcher with default params
-    bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1,des2, k=2)
+        root.config(menu=self.menubar)
 
-    # Apply ratio test
-    good = []
-    for m,n in matches:
-        if m.distance < 0.75*n.distance:
-            good.append([m])
+    def traitement(self, img, imgQuery):
+        # Initiate SIFT detector
+        sift = cv2.xfeatures2d.SIFT_create()
 
-    # cv2.drawMatchesKnn expects list of lists as matches.
-    img3 = cv2.drawMatchesKnn(img,kp1,imgQuery,kp2,good,None,flags=2)
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(img,None)
+        kp2, des2 = sift.detectAndCompute(imgQuery,None)
 
-    return img3, good, matches
+        # BFMatcher with default params
+        bf = cv2.BFMatcher()
+        matches = bf.knnMatch(des1,des2, k=2)
 
-def chooseImage():
-    filepath = askopenfilename(title="Ouvrir une image",
-                               filetypes=[('img files', '.png .jpg .jpeg'), ('all files', '.*')])
-    if filepath == '':
-        return None
-    return filepath
+        # Apply ratio test
+        good = []
+        for m,n in matches:
+            if m.distance < 0.75*n.distance:
+                good.append([m])
 
-def findMeme(filepath):
-    allRef = []
+        # cv2.drawMatchesKnn expects list of lists as matches.
+        img3 = cv2.drawMatchesKnn(img,kp1,imgQuery,kp2,good,None,flags=2)
 
-    os.chdir("./img/templates")
-    for file in glob.glob("*.jpg"):
-        allRef.append(os.path.abspath(file))
+        return img3, good, matches
 
-    if filepath is None:
-        return None
-    img = cv2.imread(filepath);
+    def chooseImage(self):
+        filepath = askopenfilename(title="Ouvrir une image",
+                                   filetypes=[('img files', '.png .jpg .jpeg'), ('all files', '.*')])
+        if filepath == '':
+            return None
+        return filepath
 
-    bestResult = [None, [], []]
+    def findMeme(self, filepath):
+        allRef = []
 
-    for template in allRef:
-        imgQuery = cv2.imread(template, 0)
-        img3, good, matches = traitement(img, imgQuery)
-        if bestResult[1].__len__() < good.__len__():
-            bestResult[0] = img3
-            bestResult[1] = good
-            bestResult[2] = matches
-    return bestResult
+        os.chdir("./img/templates")
+        for file in glob.glob("*.jpg"):
+            allRef.append(os.path.abspath(file))
 
-def work():
-    filepath = chooseImage()
-    result = findMeme(filepath)
+        if filepath is None:
+            return None
+        img = cv2.imread(filepath);
 
-    im = Image.fromarray(result[0])
-    imgtk = tki.PhotoImage(image=im)
+        bestResult = [None, [], []]
 
-    # Put it in the display window
-    labelImage.imgtk = imgtk;
-    labelImage.config(image=imgtk)
+        for template in allRef:
+            imgQuery = cv2.imread(template, 0)
+            img3, good, matches = self.traitement(img, imgQuery)
+            if bestResult[1].__len__() < good.__len__():
+                bestResult[0] = img3
+                bestResult[1] = good
+                bestResult[2] = matches
+        return bestResult
+
+    def work(self):
+        filepath = self.chooseImage()
+        result = self.findMeme(filepath)
+
+        im = Image.fromarray(result[0])
+        imgtk = tki.PhotoImage(image=im)
+
+        # Put it in the display window
+        self.labelImage.imgtk = imgtk;
+        self.labelImage.config(image=imgtk)
+
+    # Ouvrir la page KYM du meme identifié
+    def knowYourDank(self, bestResult):
+        webbrowser.open('www.knowyourmeme.com/memes/')
 
 
 if __name__ == "__main__":
     fenetre = tk.Tk()
     fenetre.title('MemeID')
 
-    #Barre de menu
-    menubar = Menu(fenetre)
-    menu1 = Menu(menubar, tearoff=0)
-    menu1.add_command(label="Ouvrir...", command=work)
-    menu1.add_separator()
-    menu1.add_command(label="Quitter", command=fenetre.quit)
-    menubar.add_cascade(label="Fichier", menu=menu1)
-    labelImage = Label(fenetre)
-    labelImage.pack(fill=BOTH)
-
-    fenetre.config(menu=menubar)
-
-    #Image
-    photo = None
-
-    canvas = Canvas(fenetre, width=350, height=200)
-    canvas.create_image(0, 0, anchor=NW, image=photo)
-    canvas.pack()
+    MainApp(fenetre)
 
     # plt.imshow(bestResult[0])
     # plt.show()
